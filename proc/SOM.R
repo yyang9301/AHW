bcd00478f49f41e3869b6105df471763cfa78d59
schrob040 <wiederweiter@gmail.com>
2017-03-08 19:20:37 +0200

    The data required for the SOM

--- proc/SOM.R
+++ proc/SOM.R
@@ -3,13 +3,15 @@
 ## This script does:
 
 # 1. Load required data
-# 2. The complete function for creating synoptic figures
+# 2. Run SOM and have a peak
+# 3. Unscale SOM results for plotting
 
 ## DEPENDS ON:
 
 library(kohonen)
 library(ggplot2)
-# library(reshape2)
+library(plyr)
+library(dplyr)
 library(data.table)
 library(doMC); registerDoMC(cores = 4)
 
@@ -22,87 +24,92 @@ library(doMC); registerDoMC(cores = 4)
 #############################################################################
 
 
-## Initial tutorial from: http://www.shanelynn.ie/self-organising-maps-for-customer-segmentation-using-r/
-
-
 # 1. Load required data ---------------------------------------------------
 
-file_idx <- dir("data/SOM", full.names = TRUE)
+# The files
+file_idx <- data.frame(file = dir("data/SOM", full.names = TRUE),
+                       x = length(dir("data/SOM")))
 
-BRAN <- data.frame()
-ERA <- data.frame()
-for(i in 1:10){
-  load(file_idx[i])
-  BRAN_temp <- merge(SOM_packet$BRAN_temp, SOM_packet$BRAN_temp_anom, by = c("x", "y"))
-  BRAN_temp$type <- NULL 
-  BRAN_temp$x <- round(BRAN_temp$x, 1)
-  BRAN_temp$y <- round(BRAN_temp$y, 1)
-  BRAN_temp <- unique(BRAN_temp)
-  
-  
+# A helper function for the following function
+col.shimmy <- function(df, var = "blank"){
+  colnames(df)[3] <- "value"
+  df$var <- rep(var, nrow(df))
+  return(df)
 }
-BRAN_temp$type <- NULL
-BRAN_temp$x <- round(BRAN_temp$x, 1)
-BRAN_temp$y <- round(BRAN_temp$y, 1)
-BRAN_temp$coords <- paste0(BRAN_temp$x, "_" , BRAN_temp$y) # Create an index column of sorts
 
-# Choose SOM training variables
-data_train <- BRAN_temp[3]
-
-data_train_matrix <- as.matrix(scale(data_train))
-names(data_train_matrix) <- names(data_train)
-
-
-file_idx <- dir("data/SOM", full.names = TRUE)
-# load(file_idx[1])
-# BRAN_temp <- SOM_packet$BRAN_temp
-BRAN_temp <- data.frame()
-# i=2
-for(i in 1:10){
-  load(file_idx[i])
-  BRAN <- SOM_packet$BRAN_temp
-  BRAN$x <- round(BRAN$x)
-  BRAN$y <- round(BRAN$y)
-  BRAN <- BRAN[, .(temp = mean(temp, na.rm = TRUE)),
-                   by = .(x,y)]
-  BRAN$coords <- paste0(BRAN$x, "_" , BRAN$y)
-  BRAN_wide <- t(BRAN$temp)
-  # names(BRAN_wide) <- names(BRAN$coords)
-  BRAN_temp <- rbind(BRAN_temp, BRAN_wide)
-  # colnames(BRAN_temp) <- BRAN$coords
+# Function for loading and formatting data for use with SOMs
+# df <- file_idx[1,] # tester...
+load.data.packet <- function(df){
+  df2 <- as.character(df$file)
+  load(df2)
+  # Combine all BRAN data
+  res <- col.shimmy(SOM_packet$BRAN_temp, "BRAN/temp")
+  res <- rbind(res, col.shimmy(SOM_packet$BRAN_uv[,c(1:3)], "BRAN/u"))
+  res <- rbind(res, col.shimmy(SOM_packet$BRAN_uv[,c(1:2,4)], "BRAN/v"))
+  res <- rbind(res, col.shimmy(SOM_packet$BRAN_temp_anom, "BRAN/temp-anom"))
+  res <- rbind(res, col.shimmy(SOM_packet$BRAN_uv_anom[,c(1:3)], "BRAN/u-anom"))
+  res <- rbind(res, col.shimmy(SOM_packet$BRAN_uv_anom[,c(1:2,4)], "BRAN/v-anom"))
+  # Then the ERA data all in one data frame
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_temp, "ERA/temp"))
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_uv[,c(1:3)], "ERA/u"))
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_uv[,c(1:2,4)], "ERA/v"))
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_temp_anom, "ERA/temp-anom"))
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_uv_anom[,c(1:3)], "ERA/u-anom"))
+  res <- rbind(res, col.shimmy(SOM_packet$ERA_uv_anom[,c(1:2,4)], "ERA/v-anom"))
+  # Round x/y for better colum names
+  res$x <- round(res$x,2) # Have to use second decimal place as first decimal rounding creates possible duplicate entries
+  res$y <- round(res$y,2)
+  # Remove any possible duplicates. SHould be none.
+  res <- res[, .(value = mean(value, na.rm = TRUE)),
+               by = .(x,y, var)]
+  res$coords <- paste0(res$x, "_" , res$y, "_", res$var)
+  res_wide <- t(res$value)
+  colnames(res_wide) <- res$coords
+  # res_wide_matrix <- scale(res_wide) # Can't run on only a single row... obviously
+  return(res_wide)
 }
-colnames(BRAN_temp) <- BRAN$coords
-BRAN_temp_matrix <- as.matrix(scale(BRAN_temp))
 
-som_grid <- somgrid(xdim = 2, ydim = 2, topo="hexagonal")
+# Load the synoptic data for only the first ten events for now
+system.time(data_packet <- ddply(file_idx[1:10,], .(file), load.data.packet, .parallel = T)) # 25 seconds
+row.names(data_packet) <- sapply(strsplit(basename(as.character(data_packet$file)), ".Rdata"),  "[[", 1)
+data_packet$file <- NULL
 
-system.time(som_model <- som(BRAN_temp_matrix, 
-                             grid=som_grid, 
-                             rlen=100, 
-                             alpha=c(0.05,0.01), 
+
+# 2. Run SOM and have a peak ----------------------------------------------
+
+# Create a scaled matrix for the SOM
+system.time(data_packet_matrix <- as.matrix(scale(data_packet))) # 6 seconds
+
+# Create the grid that the SOM will use to determine the number of nodes
+som_grid <- somgrid(xdim = 2, ydim = 2, topo="hexagonal") # This grid looks for the best 4 nodes
+
+# Run the SOM
+system.time(som_model <- som(data_packet_matrix, # 17 seconds
+                             grid = som_grid, 
+                             rlen = 100, 
+                             alpha = c(0.05,0.01), 
                              n.hood = "circular",
                              keep.data = TRUE ))
 
 # Visualise
 plot(som_model, type = "changes")
-#counts within nodes
-plot(som_model, type = "counts", main="Node Counts")
-#map quality
-plot(som_model, type = "quality", main="Node Quality/Distance")
-#neighbour distances
-plot(som_model, type="dist.neighbours", main = "SOM neighbour distances", palette.name=grey.colors)
-#code spread
-plot(som_model, type = "codes")
+# Counts within nodes
+plot(som_model, type = "counts", main = "Node Counts")
+# Map quality
+plot(som_model, type = "quality", main = "Node Quality/Distance")
+# Neighbour distances
+plot(som_model, type="dist.neighbours", main = "SOM neighbour distances", palette.name = grey.colors)
+# Code spread
+# plot(som_model, type = "codes") # Don't run this as it is too beefy due to all of the pixels
 
-# show the WCSS metric for kmeans for different clustering sizes.
-# Can be used as a "rough" indicator of the ideal number of clusters
-mydata <- som_model$codes
-wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
-for (i in 1:3) wss[i] <- sum(kmeans(mydata,
+# The WCSS metric for different kmeans clustering
+codes <- som_model$codes
+wss <- (nrow(codes)-1)*sum(apply(codes,2,var))
+for (i in 2:nrow(codes)-1) wss[i] <- sum(kmeans(codes,
                                      centers=i)$withinss)
-par(mar=c(5.1,4.1,4.1,2.1))
-plot(1:3, wss, type="b", xlab="Number of Clusters",
-     ylab="Within groups sum of squares", main="Within cluster sum of squares (WCSS)")
+par(mar = c(5.1,4.1,4.1,2.1))
+plot(1:(nrow(codes)-1), wss, type = "b", xlab = "Kmeans Clusters",
+     ylab = "Within groups sum of squares", main = "Within cluster sum of squares (WCSS)")
 
 # Form clusters on grid
 ## use hierarchical clustering to cluster the codebook vectors
@@ -112,34 +119,36 @@ som_cluster <- cutree(hclust(dist(som_model$codes)), 2)
 plot(som_model, type="mapping", bgcol = som_cluster, main = "Clusters")
 add.cluster.boundaries(som_model, som_cluster)
 
-#show the same plot with the codes instead of just colours
-plot(som_model, type="codes", bgcol = som_cluster, main = "Clusters")
-add.cluster.boundaries(som_model, som_cluster)
+# Show the same plot with the codes instead of just colours
+# plot(som_model, type="codes", bgcol = som_cluster, main = "Clusters") # This is too beefy...
+# add.cluster.boundaries(som_model, som_cluster)
 
 
-#### Extract nodes and plot #####
+# 3. Unscale SOM results for plotting -------------------------------------
 
-test <- som_model$codes[1,]
-test <- as.data.frame(test)
-# test$coords <- rownames(test)
-test$x <- as.numeric(sapply(strsplit(rownames(test), "_"), "[[", 1))
-test$y <- as.numeric(sapply(strsplit(rownames(test), "_"), "[[", 2))
+# Melt data_packet
+data_packet$event <- rownames(data_packet)
+data_packet_long <- melt(data_packet, id = "event")
 
-ggplot(data = test, aes(x = x, y = y, fill = test)) +
-  geom_raster()
+# Determine which event goes in which node
+event_node <- data.frame(event = data_packet$event, node = som_model$unit.classif)
 
-# Unscale the results
-var_unscaled <- data.frame()
-for(i in 1:length(BRAN_temp)){
-  var_1 <- as.data.frame(aggregate(as.numeric(BRAN_temp[,i]), by = list(som_model$unit.classif), FUN = mean, simplify = TRUE))
-  colnames(var_1) <- c("node", "temp")
-  var_1$x <- rep(as.numeric(sapply(strsplit(colnames(BRAN_temp[i]), "_"), "[[", 1)), nrow(var_1))
-  var_1$y <- rep(as.numeric(sapply(strsplit(colnames(BRAN_temp[i]), "_"), "[[", 2)))
-  var_unscaled <- rbind(var_unscaled, var_1)
-}
+data_packet_long <- data_packet_long %>%
+  group_by(event) %>%
+  mutate(node = event_node$node[event_node$event == event][1])
+data_packet_long <- data.table(data_packet_long)
 
-ggplot(data = var_unscaled, aes(x = x, y = y, fill = temp)) +
+# Create the mean values that serve as the unscaled results from the SOM
+var_unscaled <- data_packet_long[, .(value = mean(value, na.rm = TRUE)),
+                             by = .(node, variable)]
+var_unscaled$x <- as.numeric(sapply(strsplit(as.character(var_unscaled$variable), "_"), "[[", 1))
+var_unscaled$y <- as.numeric(sapply(strsplit(as.character(var_unscaled$variable), "_"), "[[", 2))
+var_unscaled$var <- sapply(strsplit(as.character(var_unscaled$variable), "_"), "[[", 3)
+var_unscaled$variable <- NULL
+
+# Test plot
+ggplot(data = var_unscaled, aes(x = x, y = y, fill = value)) +
   geom_raster() +
-  facet_wrap(~node)
+  facet_grid(var~node)
 
 
